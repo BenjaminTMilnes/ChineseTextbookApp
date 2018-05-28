70c6cde75a44e57aaeaf3294af4ca1bff3da0ef2
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2018-05-28 12:39:13 +0100

    Slight changes to content ideas

--- ChineseTextbookApp.WebApplication/application.js
+++ ChineseTextbookApp.WebApplication/application.js
@@ -1,14 +1,24 @@
 ﻿var application = angular.module("ChineseTextbookApp", []);
 
-var vocabulary = [{ "word": "你", "choices": ["You", "I", "He"] },
+var vocabulary = [
 { "word": "我", "choices": ["I", "You", "She"] },
-{ "word": "他", "choices": ["He", "She", "They"] },
-{ "word": "她", "choices": ["She", "He", "You"] },
+{ "word": "你", "choices": ["You", "I", "He"] },
+{ "word": "您", "choices": ["You (formal)", "You (informal)", "I"] },
+{ "word": "他", "choices": ["He", "She", "You"] },
+{ "word": "她", "choices": ["She", "He", "They"] },
+{ "word": "好", "choices": ["good", "very", "are"] },
+{ "word": "吗", "choices": ["[question particle]", "[follow-up particle]", "good"] },
+{ "word": "很", "choices": ["very", "good", "you"] },
+{ "word": "呢", "choices": ["[follow-up particle]", "[question particle]", "good"] }
+];
+
+var phrases = [
 { "word": "你好", "choices": ["Hello", "How are you?", "Good morning"] },
 { "word": "你好吗", "choices": ["How are you?", "Hello", "I'm good"] },
-{ "word": "您好", "choices": ["How do you do?", "How are you?", "Hello"] },
+{ "word": "您好", "choices": ["How do you do.", "How are you?", "Hello"] },
 { "word": "我很好", "choices": ["I'm very well.", "How are you?", "I'm happy."] },
-{ "word": "你呢", "choices": ["And you?", "Hello", "How are you?"] }];
+{ "word": "你呢", "choices": ["And you?", "Hello", "How are you?"] }
+];
 
 application.controller("MainController", ["$scope", "$timeout", function MainController($scope, $timeout) {
 
