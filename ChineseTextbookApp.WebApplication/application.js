d74a4789bce7b1c6f99de4c6a171b148d51c03c4
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2018-12-28 23:44:52 +0000

    Began adding match-the-pairs activity

--- ChineseTextbookApp.WebApplication/application.js
+++ ChineseTextbookApp.WebApplication/application.js
@@ -1,5 +1,22 @@
 ﻿var application = angular.module("ChineseTextbookApp", []);
 
+function orderRandomly(array) {
+    var currentIndex = array.length;
+    var randomIndex;
+    var temporaryValue;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
 var vocabulary = [
 { "word": "我", "choices": ["I", "You", "She"] },
 { "word": "你", "choices": ["You", "I", "He"] },
@@ -19,7 +36,6 @@ var phrases = [
 { "word": "我很好", "choices": ["I'm very well.", "How are you?", "I'm happy."] },
 { "word": "你呢", "choices": ["And you?", "Hello", "How are you?"] }
 ];
-
 application.controller("MainController", ["$scope", "$timeout", function MainController($scope, $timeout) {
 
     $scope.index = -1;
@@ -27,6 +43,14 @@ application.controller("MainController", ["$scope", "$timeout", function MainCon
     $scope.currentChoices = [];
     $scope.correctAnswer = "";
     $scope.showCorrect = false;
+
+    $scope.currentWords = [];
+
+    var v1 = vocabulary.slice(0, 8);
+
+    $scope.currentWords = v1.map(w => ( { "word": w.word, "language":"Chinese", "style":"matchpairs-word-chinese"}));
+    $scope.currentWords = $scope.currentWords.concat(v1.map(w => ({ "word": w.choices[0], "language": "English", "style": "matchpairs-word-english" })));
+    $scope.currentWords = orderRandomly($scope.currentWords);
 
     $scope.getNextQuestion = function () {
         if ($scope.index < vocabulary.length) {
