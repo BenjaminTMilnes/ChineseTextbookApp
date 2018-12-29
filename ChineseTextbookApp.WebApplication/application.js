ebccb318766cc60bbe969c5acf6448472a13f58b
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2018-12-29 13:11:55 +0000

    Moved match-the-pairs activity into separate files

--- ChineseTextbookApp.WebApplication/application.js
+++ ChineseTextbookApp.WebApplication/application.js
@@ -17,22 +17,6 @@ function orderRandomly(array) {
     return array;
 }
 
-function getVocabularyPairs(vocabularySet, numberOfPairs) {
-    var words = [];
-
-    var v = orderRandomly(vocabularySet);
-    v = v.slice(0, numberOfPairs);
-
-    for (let w of v) {
-        words.push({ "word": w.word, "language": "Chinese", "style": "matchpairs-word-chinese", "colour": "blue" });
-        words.push({ "word": w.choices[0], "language": "English", "style": "matchpairs-word-english", "colour": "blue" });
-    }
-
-    words = orderRandomly(words);
-
-    return words;
-}
-
 var vocabulary = [
 { "word": "我", "choices": ["I", "You", "She"] },
 { "word": "你", "choices": ["You", "I", "He"] },
@@ -60,72 +44,6 @@ application.controller("MainController", ["$scope", "$timeout", function MainCon
     $scope.currentChoices = [];
     $scope.correctAnswer = "";
     $scope.showCorrect = false;
-
-    $scope.word1 = "";
-    $scope.word2 = "";
-
-    $scope.score = 0;
-
-    $scope.startMatchingPairsActivity = function () {
-
-        $scope.currentWords = getVocabularyPairs(vocabulary, 8);
-
-        $scope.word1 = "";
-        $scope.word2 = "";
-
-        $scope.score = 0;
-
-    }
-
-    $scope.startMatchingPairsActivity();
-
-    $scope.chooseWord = function (word) {
-        if ($scope.word1 != "" && $scope.word2 != "") {
-            return;
-        }
-
-        for (let w of $scope.currentWords) {
-            if (w.word == word) {
-                if (w.colour == "green") {
-                    return;
-                }
-
-                w.colour = "red";
-            }
-        }
-
-        if ($scope.word1 == "") {
-            $scope.word1 = word;
-            return;
-        }
-
-        $scope.word2 = word;
-
-        for (let w1 of vocabulary) {
-            if ((w1.word == $scope.word1 && w1.choices[0] == $scope.word2) || (w1.word == $scope.word2 && w1.choices[0] == $scope.word1)) {
-
-                $scope.currentWords.forEach(w => { if (w.word == $scope.word1 || w.word == $scope.word2) { w.colour = "green"; } });
-
-                $scope.word1 = "";
-                $scope.word2 = "";
-
-                $scope.score += 1;
-
-                if ($scope.score >= 8) {
-                    $timeout($scope.startMatchingPairsActivity, 1000);
-                }
-
-                return;
-            }
-        }
-
-        $timeout(function () {
-            $scope.currentWords.forEach(w => { if (w.word == $scope.word1 || w.word == $scope.word2) { w.colour = "blue"; } });
-
-            $scope.word1 = "";
-            $scope.word2 = "";
-        }, 500);
-    }
 
     $scope.getNextQuestion = function () {
         if ($scope.index < vocabulary.length) {
---
+++ ChineseTextbookApp.WebApplication/matchthepairs.js
@@ -0,0 +1,87 @@
+﻿
+
+function getVocabularyPairs(vocabularySet, numberOfPairs) {
+    var words = [];
+
+    var v = orderRandomly(vocabularySet);
+    v = v.slice(0, numberOfPairs);
+
+    for (let w of v) {
+        words.push({ "word": w.word, "language": "Chinese", "style": "matchthepairs-word-chinese", "colour": "blue" });
+        words.push({ "word": w.choices[0], "language": "English", "style": "matchthepairs-word-english", "colour": "blue" });
+    }
+
+    words = orderRandomly(words);
+
+    return words;
+}
+
+application.controller("MatchThePairsController", ["$scope", "$timeout", function MatchThePairsController($scope, $timeout) {
+
+    $scope.word1 = "";
+    $scope.word2 = "";
+
+    $scope.score = 0;
+
+    $scope.startActivity = function () {
+
+        $scope.currentWords = getVocabularyPairs(vocabulary, 8);
+
+        $scope.word1 = "";
+        $scope.word2 = "";
+
+        $scope.score = 0;
+
+    }
+
+    $scope.startActivity();
+
+    $scope.chooseWord = function (word) {
+        if ($scope.word1 != "" && $scope.word2 != "") {
+            return;
+        }
+
+        for (let w of $scope.currentWords) {
+            if (w.word == word) {
+                if (w.colour == "green") {
+                    return;
+                }
+
+                w.colour = "red";
+            }
+        }
+
+        if ($scope.word1 == "") {
+            $scope.word1 = word;
+            return;
+        }
+
+        $scope.word2 = word;
+
+        for (let w1 of vocabulary) {
+            if ((w1.word == $scope.word1 && w1.choices[0] == $scope.word2) || (w1.word == $scope.word2 && w1.choices[0] == $scope.word1)) {
+
+                $scope.currentWords.forEach(w => { if (w.word == $scope.word1 || w.word == $scope.word2) { w.colour = "green"; } });
+
+                $scope.word1 = "";
+                $scope.word2 = "";
+
+                $scope.score += 1;
+
+                if ($scope.score >= 8) {
+                    $timeout($scope.startActivity, 1000);
+                }
+
+                return;
+            }
+        }
+
+        $timeout(function () {
+            $scope.currentWords.forEach(w => { if (w.word == $scope.word1 || w.word == $scope.word2) { w.colour = "blue"; } });
+
+            $scope.word1 = "";
+            $scope.word2 = "";
+        }, 500);
+    }
+
+}]);
