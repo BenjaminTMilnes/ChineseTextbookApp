bf6baa6d2071a4a4b55d31807557e0d8debe8799
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2018-12-29 16:54:27 +0000

    Moved multiple choice activity code into separate files

--- ChineseTextbookApp.WebApplication/application.js
+++ ChineseTextbookApp.WebApplication/application.js
@@ -1,4 +1,10 @@
-﻿var application = angular.module("ChineseTextbookApp", []);
+﻿var application = angular.module("ChineseTextbookApp", ["ngRoute", "ngSanitize"]);
+
+application.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", { templateUrl: "multiplechoice.html", controller: "MultipleChoiceController" })
+    .when("/matchthepairs", { templateUrl: "matchthepairs.html", controller: "MatchThePairsController" });
+});
 
 function orderRandomly(array) {
     var currentIndex = array.length;
@@ -17,17 +23,22 @@ function orderRandomly(array) {
     return array;
 }
 
-var vocabulary = [
-{ "word": "我", "choices": ["I", "You", "She"] },
-{ "word": "你", "choices": ["You", "I", "He"] },
-{ "word": "您", "choices": ["You (formal)", "You (informal)", "I"] },
-{ "word": "他", "choices": ["He", "She", "You"] },
-{ "word": "她", "choices": ["She", "He", "They"] },
-{ "word": "好", "choices": ["good", "very", "are"] },
-{ "word": "吗", "choices": ["[question particle]", "[follow-up particle]", "good"] },
-{ "word": "很", "choices": ["very", "good", "you"] },
-{ "word": "呢", "choices": ["[follow-up particle]", "[question particle]", "good"] }
-];
+var vocabularySet1 = {
+    "multipleChoiceTitle": "Choose the correct English word for the following Chinese word.",
+    "from": "Chinese",
+    "to": "English",
+    "vocabulary": [
+        { "word": "我", "choices": ["I", "You", "He", "She", "We"] },
+        { "word": "你", "choices": ["You", "I", "He", "She", "We"] },
+        { "word": "您", "choices": ["You (formal)", "You (informal)", "I"] },
+        { "word": "他", "choices": ["He", "She", "You"] },
+        { "word": "她", "choices": ["She", "He", "They"] },
+        { "word": "好", "choices": ["good", "very", "are"] },
+        { "word": "吗", "choices": ["[question particle]", "[follow-up particle]", "good"] },
+        { "word": "很", "choices": ["very", "good", "you"] },
+        { "word": "呢", "choices": ["[follow-up particle]", "[question particle]", "good"] }
+    ]
+};
 
 var phrases = [
 { "word": "你好", "choices": ["Hello", "How are you?", "Good morning"] },
@@ -39,34 +50,5 @@ var phrases = [
 
 application.controller("MainController", ["$scope", "$timeout", function MainController($scope, $timeout) {
 
-    $scope.index = -1;
-    $scope.currentWord = "";
-    $scope.currentChoices = [];
-    $scope.correctAnswer = "";
-    $scope.showCorrect = false;
-
-    $scope.getNextQuestion = function () {
-        if ($scope.index < vocabulary.length) {
-            $scope.index++;
-        }
-        else {
-            $scope.index = 0;
-        }
-
-        $scope.currentWord = vocabulary[$scope.index].word;
-        $scope.currentChoices = vocabulary[$scope.index].choices;
-        $scope.correctAnswer = vocabulary[$scope.index].choices[0];
-
-        $scope.showCorrect = false;
-    }
-
-    $scope.getNextQuestion();
-
-    $scope.chooseAnswer = function (answer) {
-        if (answer == $scope.correctAnswer) {
-            $scope.showCorrect = true;
-            $timeout($scope.getNextQuestion, 1000);
-        }
-    }
 
 }]);
---
+++ ChineseTextbookApp.WebApplication/multiplechoice.js
@@ -0,0 +1,38 @@
+﻿
+application.controller("MultipleChoiceController", ["$scope", "$timeout", function MultipleChoiceController($scope, $timeout) {
+
+    $scope.index = -1;
+    $scope.currentWord = "";
+    $scope.currentChoices = [];
+    $scope.correctAnswer = "";
+    $scope.showCorrect = false;
+    $scope.title = vocabularySet1.multipleChoiceTitle;
+
+    $scope.getNextQuestion = function () {
+
+        var vocabulary = vocabularySet1.vocabulary;
+
+        if ($scope.index < vocabulary.length) {
+            $scope.index++;
+        }
+        else {
+            $scope.index = 0;
+        }
+
+        $scope.currentWord = vocabulary[$scope.index].word;
+        $scope.correctAnswer = vocabulary[$scope.index].choices[0];
+        $scope.currentChoices = orderRandomly(vocabulary[$scope.index].choices);
+
+        $scope.showCorrect = false;
+    }
+
+    $scope.getNextQuestion();
+
+    $scope.chooseAnswer = function (answer) {
+        if (answer == $scope.correctAnswer) {
+            $scope.showCorrect = true;
+            $timeout($scope.getNextQuestion, 1000);
+        }
+    }
+
+}]);
